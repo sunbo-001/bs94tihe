d258f7e36d59a75c8cb0e23f611a7638fcb8cfe7
bo <sunbotju@outlook.de>
2020-08-01 23:20:30 -0700

    create a layout login

--- view.py
+++ view.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 def index(request):
    return HttpResponse('index')
 
-a = 1
+def login(request):
+   return redirect('/index')	
